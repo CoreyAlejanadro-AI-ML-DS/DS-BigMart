f385ec5137c6ba0b075d01acfaca226f5b0298ac
coreyalejandro <92589532+coreyalejandro@users.noreply.github.com>
2023-10-01 07:22:42 -0500

    Initial commit with Cookiecutter template

---
+++ DS-BigMart/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My take on the well-known BigMart dataset and machine learning challenge projectt',
+    author='Corey Alejandro',
+    license='MIT',
+)
